8259ad1f88496beca310a87535e75affc35d860b
Mr.Midnight <118108435+DrNexel@users.noreply.github.com>
2025-02-03 14:43:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
+  packname: 'spiderverse',
+  author: 'MrMaiden',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'MrMaiden', // Your name
+  ownerNumber: '2349080826205', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
